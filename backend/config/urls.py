642b490b96c75f88d0ea3f05cbf3e74b339d5f73
igor20192 <igor.udovenko2012@meta.ua>
2025-05-02 14:01:21 +0300

    add path : apps.apartments.api.urls, apps.users.api.urls_auth

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,16 +4,23 @@ from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
-    SpectacularRedocView
+    SpectacularRedocView,
 )
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/users/', include('apps.users.api.urls')),
-
+    path("admin/", admin.site.urls),
+    path("api/v1/users/", include("apps.users.api.urls")),
+    path("api/v1/apartments/", include("apps.apartments.api.urls")),
+    path("api/v1/auth/", include("apps.users.api.urls_auth")),
     # Documentation
-    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/v1/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/v1/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+    ),
 ]
