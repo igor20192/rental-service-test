ba2817e90882c9667c9a2bd9d64b1bc9e2d462cf
igor20192 <igor.udovenko2012@meta.ua>
2025-05-02 20:35:44 +0300

    add frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-blue-600 text-4xl font-bold">Tailwind работает! 🎉</h1>
+    </div>
+  );
+}
+
