0d59023225d45751ef045fd6dcba4ceb91071566
igor20192 <igor.udovenko2012@meta.ua>
2025-05-05 14:26:49 +0300

    add docstring

--- backend/apps/apartments/api/views.py
+++ backend/apps/apartments/api/views.py
@@ -13,9 +13,13 @@ from apps.apartments.api.permissions import IsOwnerOrReadOnly
 logger = logging.getLogger(__name__)
 
 
-@method_decorator(cache_page(60 * 5), name="list")  # 5 минут кеш
-@method_decorator(cache_page(60 * 5), name="retrieve")  # 5 минут кеш
+@method_decorator(cache_page(60 * 5), name="list")
+@method_decorator(cache_page(60 * 5), name="retrieve")
 class ApartmentViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing apartments.
+    """
+
     serializer_class = ApartmentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     lookup_field = "slug"
@@ -24,6 +28,12 @@ class ApartmentViewSet(viewsets.ModelViewSet):
     search_fields = ["name", "description"]
 
     def get_queryset(self):
+        """
+        Returns a queryset of apartments with optional filtering by price.
+
+        Supports filtering by minimum and maximum price through the query parameters
+        `price_min` and `price_max` respectively.
+        """
         queryset = Apartment.objects.select_related("owner").only(
             "id",
             "name",
@@ -46,6 +56,12 @@ class ApartmentViewSet(viewsets.ModelViewSet):
         return queryset
 
     def perform_create(self, serializer):
+        """
+        Creates a new apartment.
+
+        Automatically sets the owner of the apartment to the current user.
+        Handles potential exceptions during creation.
+        """
         try:
             serializer.save(owner=self.request.user)
             logger.info(f"Apartment created by {self.request.user.email}")
@@ -54,6 +70,12 @@ class ApartmentViewSet(viewsets.ModelViewSet):
             raise APIException("Error creating apartment")
 
     def perform_update(self, serializer):
+        """
+        Updates an existing apartment.
+
+        Handles potential exceptions during the update. The owner cannot be changed,
+        as it is set during creation.
+        """
         try:
             serializer.save()
             logger.info(f"Apartment updated by {self.request.user.email}")
@@ -62,6 +84,11 @@ class ApartmentViewSet(viewsets.ModelViewSet):
             raise APIException("Error updating apartment")
 
     def perform_destroy(self, instance):
+        """
+        Deletes an apartment.
+
+        Handles potential exceptions during deletion.
+        """
         try:
             logger.info(f"Apartment deleted by {self.request.user.email}")
             instance.delete()
